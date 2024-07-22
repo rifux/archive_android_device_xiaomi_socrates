7b96d36511cfaff727e999d5ae2372b1ecb5c95f
Blinkov `rifux` Vladimir ♪ <87264036+rifux@users.noreply.github.com>
2024-07-22 17:49:21 +0300

    use patched lineage makefiles instead

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,17 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-#PRODUCT_MAKEFILES := \
-#    $(LOCAL_DIR)/lineage_socrates.mk \
-#    $(LOCAL_DIR)/rising_socrates.mk
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rising_socrates.mk
+    $(LOCAL_DIR)/lineage_socrates.mk
 
 COMMON_LUNCH_CHOICES := \
-    rising_socrates-eng \
-    rising_socrates-user \
-    rising_socrates-userdebug
-#    lineage_socrates-eng \
-#    lineage_socrates-user \
-#    lineage_socrates-userdebug
+    lineage_socrates-eng \
+    lineage_socrates-user \
+    lineage_socrates-userdebug
