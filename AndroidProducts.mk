7f082be05ab5cfbb8d47fee28fc6f4f520507926
Blinkov `rifux` Vladimir ♪ <87264036+rifux@users.noreply.github.com>
2024-07-22 17:35:37 +0300

    trying to make rising_socrates.mk default

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,14 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+#PRODUCT_MAKEFILES := \
+#    $(LOCAL_DIR)/lineage_socrates.mk \
+#    $(LOCAL_DIR)/rising_socrates.mk
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_socrates.mk \
     $(LOCAL_DIR)/rising_socrates.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_socrates-eng \
-    lineage_socrates-user \
-    lineage_socrates-userdebug \
     rising_socrates-eng \
     rising_socrates-user \
     rising_socrates-userdebug
+#    lineage_socrates-eng \
+#    lineage_socrates-user \
+#    lineage_socrates-userdebug
