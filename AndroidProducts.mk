8bed4350756ffb487157e1337c2772ee8004f823
Blinkov `rifux` Vladimir ♪ <87264036+rifux@users.noreply.github.com>
2024-07-31 16:36:32 +0300

    socrates: EverestOS initial support
    Signed-off-by: Blinkov `rifux` Vladimir ♪ <87264036+rifux@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project, Blinkov `rifux` Vladimir ♪
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_socrates.mk
+    $(LOCAL_DIR)/everest_socrates.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_socrates-eng \
-    lineage_socrates-user \
-    lineage_socrates-userdebug
+    everest_socrates-eng \
+    everest_socrates-user \
+    everest_socrates-userdebug
--- everest_socrates.mk
+++ everest_socrates.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project, Blinkov `rifux` Vladimir ♪
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -14,10 +14,23 @@ $(call inherit-product, device/xiaomi/socrates/device.mk)
 # Inherit LineageOS configurations
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_socrates
+# AOSP flags
+PRODUCT_NAME := everest_socrates
 PRODUCT_DEVICE := socrates
 PRODUCT_MODEL := Redmi K60 Pro
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_FINGERPRINT := Redmi/socrates/socrates:14/UKQ1.230804.001/V816.0.9.0.UMKCNXM:user/release-keys
+
+# EverestOS flags
+EVEREST_MAINTAINER := "rifux.dev"
+TARGET_SUPPORTS_BLUR := true
+TARGET_HAS_UDFPS := true
+TARGET_BOOT_ANIMATION_RES := 1440
+EXTRA_UDFPS_ANIMATIONS := true
+
+# EverestOS build type flags
+# | Vanilla
+WITH_GAPPS := false
+TARGET_INCLUDE_PIXEL_LAUNCHER := false
