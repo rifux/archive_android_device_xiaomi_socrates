d42f0f1210befb08d985cd5aa6dbeb346171647c
Blinkov `rifux` Vladimir ♪ <87264036+rifux@users.noreply.github.com>
2024-08-04 18:26:54 +0300

    socrates: RisingOS adaptation
    Signed-off-by: Blinkov `rifux` Vladimir ♪ <87264036+rifux@users.noreply.github.com>

--- lineage_socrates.mk
+++ lineage_socrates.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project, Vladimir `rifux` Blinkov
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -14,6 +14,7 @@ $(call inherit-product, device/xiaomi/socrates/device.mk)
 # Inherit LineageOS configurations
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# AOSP common flags
 PRODUCT_NAME := lineage_socrates
 PRODUCT_DEVICE := socrates
 PRODUCT_MODEL := Redmi K60 Pro
@@ -21,3 +22,25 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_FINGERPRINT := Redmi/socrates/socrates:14/UKQ1.230804.001/V816.0.9.0.UMKCNXM:user/release-keys
+
+# RisingOS common flags
+TARGET_ENABLE_BLUR := true
+RISING_MAINTAINER := "Vladimir Blinkov"
+PRODUCT_NO_CAMERA := false
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="Snapdragon 8 Gen 2" \
+    RISING_MAINTAINER="Vladimir Blinkov" \
+    PRIVATE_BUILD_DESC="socrates-user 14 UKQ1.230804.001 V816.0.9.0.UMKCNXM release-keys"
+
+# RisingOS build flags
+# | Google
+WITH_GMS := true
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_ENABLE_PIXEL_FEATURES := true
+
+# | Vanilla
+# WITH_GMS := false
+# RISING_PACKAGE_TYPE := "VANILLA_AOSP"
+# TARGET_CORE_GMS := false
+# TARGET_CORE_GMS_EXTRAS := false
