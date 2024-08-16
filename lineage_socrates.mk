cf1deaba068be610e0105ee7bd6f81c9913fd582
Blinkov `rifux` Vladimir ♪ <contact@rifux.dev>
2024-08-16 23:05:01 +0300

    socrates: Project Matrixx initial support
    Signed-off-by: Blinkov `rifux` Vladimir ♪ <contact@rifux.dev>

--- lineage_socrates.mk
+++ lineage_socrates.mk
@@ -14,6 +14,7 @@ $(call inherit-product, device/xiaomi/socrates/device.mk)
 # Inherit LineageOS configurations
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# AOSP flags
 PRODUCT_NAME := lineage_socrates
 PRODUCT_DEVICE := socrates
 PRODUCT_MODEL := Redmi K60 Pro
@@ -21,3 +22,28 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_FINGERPRINT := Redmi/socrates/socrates:13/TKQ1.221114.001/V816.0.24.7.28.DEV:user/release-keys
+
+# Matrixx flags
+PRODUCT_SYSTEM_NAME := socrates
+PRODUCT_SYSTEM_DEVICE := socrates
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="socrates-user 13 TKQ1.221114.001 V816.0.24.7.28.DEV release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_HAS_UDFPS := true
+
+MATRIXX_BUILD_TYPE := Unofficial
+MATRIXX_MAINTAINER := "Vladimir Blinkov"
+MATRIXX_CHIPSET := "Snapdragon 8 Gen 2"
+MATRIXX_BATTERY := 5000mAh
+MATRIXX_DISPLAY := 1440x3200
+
+# Matrixx build flags
+# | Google
+WITH_GMS := true
+
+# | Vanilla
+#WITH_GMS := false
